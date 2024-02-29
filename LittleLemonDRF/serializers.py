e2c5b8be4b46bef15f0342148bf4ee51c2fb6e99
inouye165 <inouye165@gmail.com>
2024-02-29 15:42:20 -0800

    Found error in serializer: fixed

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -13,5 +13,6 @@ class RatingSerializer (serializers.ModelSerializer):
     class Meta():
         model = Rating
         fields = ['user','menuitem_id', 'rating']
-        validators = [UniqueTogetherValidator(queryset = Rating.objects.all(),fields = ['user','menuitem_id','rating'])]
+        # validators below was explained differently in te instructions than it ended up being in the examplar.  
+        validators = [UniqueTogetherValidator(queryset = Rating.objects.all(),fields = ['user', 'menuitem_id'])]
         extra_kwargs = {'rating':{'max_value':5,'min_value':0}}
