6a444912927969765be72593f65017925936af14
inouye165 <inouye165@gmail.com>
2024-02-29 15:44:22 -0800

    Found error in serializer: fixed

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -13,6 +13,9 @@ class RatingSerializer (serializers.ModelSerializer):
     class Meta():
         model = Rating
         fields = ['user','menuitem_id', 'rating']
-        # validators below was explained differently in te instructions than it ended up being in the examplar.  
+        # validators below was explained differently in the instructions than it ended up being in the examplar.  
+        # 'a fields variable that contains three list elements that are strings like: user, menuitem_id and rating  '
+        # the instructions said to add user, menuitem_id and rating the fields list, but that did not work as shown.  
+        # The examplar showed the below code.
         validators = [UniqueTogetherValidator(queryset = Rating.objects.all(),fields = ['user', 'menuitem_id'])]
         extra_kwargs = {'rating':{'max_value':5,'min_value':0}}
